53e1f1d605e6f91bb8623ff72ccce05a41403af0
Niranjan kumar <niranjankumar.gopalan@syncfusion.com>
2021-05-17 11:43:48 +0530

    UG Samples for CalendarDateRange Picker controls are updated.

---
+++ Samples/Selection/Selection.winui_net50/Selection.winui_net50/ViewModel/CalendarDateRangePickerViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Syncfusion.UI.Xaml.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection
+{
+    class CalendarDateRangePickerViewModel : NotificationObject
+    {
+        private DayOfWeek firstDayOfWeek = DayOfWeek.Sunday;
+        private UInt64 maxdaysCountInRange = 8;
+        private UInt64 mindaysCountInRange = 4;
+        private string placeHolderText = "No Date is Selected";
+
+        public UInt64 MinDaysCountInRange
+        {
+            get
+            {
+                return mindaysCountInRange;
+            }
+            set
+            {
+                if (mindaysCountInRange != value)
+                {
+                    mindaysCountInRange = value;
+                    this.RaisePropertyChanged(nameof(this.MinDaysCountInRange));
+                }
+            }
+        }
+        public UInt64 MaxDaysCountInRange
+        {
+            get
+            {
+                return maxdaysCountInRange;
+            }
+            set
+            {
+                if (maxdaysCountInRange != value)
+                {
+                    maxdaysCountInRange = value;
+                    this.RaisePropertyChanged(nameof(this.MaxDaysCountInRange));
+                }
+            }
+        }
+
+        public string PlaceHolderText
+        {
+            get
+            {
+                return placeHolderText;
+            }
+            set
+            {
+                if (placeHolderText != value)
+                {
+                    placeHolderText = value;
+                    this.RaisePropertyChanged(nameof(this.PlaceHolderText));
+                }
+            }
+        }
+
+        public DayOfWeek FirstDayOfWeek
+        {
+            get
+            {
+                return firstDayOfWeek;
+            }
+            set
+            {
+                firstDayOfWeek = value;
+                this.RaisePropertyChanged(nameof(FirstDayOfWeek));
+            }
+        }
+
+    }
+}
